a56a643a506d352b3a5ff8bd1d4f60d33bc0d822
Alesis3 <alexis.art349@hotmail.com>
2025-07-31 13:38:35 -0600

    Caida de la arena

--- juegos/sand.py
+++ juegos/sand.py
@@ -2,22 +2,59 @@
 width, height = [int(i) for i in input().split()]
 amount = int(input())
 positions = []
-for i in range(amount):
-    inputs = input().split()
-    s = inputs[0]
-    position = int(inputs[1])
-    positions.append((s, position))
+
+def is_empty(x, y):
+    if x < 0 or x >= width or y < 0 or y >= height:
+        return False
+    for sand in positions:
+        if sand[1] == x and sand[2] == y:
+            return False
+    return True
 
 
+for _ in range(amount):
+    s, col = input().split()
+    col = int(col)
+    coordinate_x, coordinate_y = col, -1
+
+    while True:
+        if is_empty(coordinate_x, coordinate_y + 1):
+            coordinate_y += 1
+            continue
+
+        moved = False
+        if s.islower():
+            if is_empty(coordinate_x + 1, coordinate_y + 1):
+                coordinate_x += 1
+                coordinate_y += 1
+                moved = True
+            elif is_empty(coordinate_x - 1, coordinate_y + 1):
+                coordinate_x -= 1
+                coordinate_y += 1
+                moved = True
+        else:
+            if is_empty(coordinate_x - 1, coordinate_y + 1):
+                coordinate_x -= 1
+                coordinate_y += 1
+                moved = True
+            elif is_empty(coordinate_x + 1, coordinate_y + 1):
+                coordinate_x += 1
+                coordinate_y += 1
+                moved = True
+
+        if not moved:
+            positions.append([s, coordinate_x, coordinate_y])
+            break
+
+#creacion del tablero
 for coordinate_y in range(height):
     print('|', end='')
     for coordinate_x in range(width):
         char = ' '
 
         for sand in positions:
-            if sand[1] == coordinate_x:
+            if sand[1] == coordinate_x and sand[2] == coordinate_y and char == ' ':
                 char = sand[0]
-
 
         print(char, end='')
 
